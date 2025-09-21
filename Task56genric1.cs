dd17deda8d1a35dd3847cd5962fcddc2be415ebe
Loki <abdohamdy4587@gmail.com>
2025-09-21 11:59:22 +0300

    Create Task56genric1.cs

---
+++ Task56genric1.cs
@@ -0,0 +1,34 @@
+﻿namespace ITI
+{
+    internal class Task1
+    {
+       class Range<T> where T : IComparable<T>
+         {
+    public T Min { get; }
+    public T Max { get; }
+
+    public Range(T min, T max)
+    {
+        if (min.CompareTo(max) > 0)
+            throw new ArgumentException("Min cannot be greater than Max.");
+
+        Min = min;
+        Max = max;
+    }
+
+    public bool IsInRange(T value)
+    {
+        return value.CompareTo(Min) >= 0 && value.CompareTo(Max) <= 0;
+    }
+
+    public dynamic Length()
+    {
+        
+        return (dynamic)Max - (dynamic)Min;
+    }
+}
+
+    }
+
+
+    }
